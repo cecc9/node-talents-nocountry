3fe5bf42c2db6a17f97ca92cad7d5b2da7d03826
cristian choque <cristianalex3401@gmail.com>
2024-07-11 15:34:04 -0500

    check fix

--- frontend/src/components/modals/Successful_Register.tsx
+++ frontend/src/components/modals/Successful_Register.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-import Modal from "../Modal";
-import semiCircle from "./greenSemiCircle.svg";
-import successMan from "./successMan.svg";
-import baseStyles from "../../styles/base-modal.module.scss";
-import Link from "next/link";
+import Image from "next/image"
+import Modal from "../Modal"
+// import semiCircle from "./greenSemiCircle.svg"
+import successMan from "./successMan.svg"
+import baseStyles from "../../styles/base-modal.module.scss"
+import Link from "next/link"
 
 function Successful_Register() {
   return (
@@ -18,19 +18,19 @@ function Successful_Register() {
       </header>
 
       <article className={baseStyles.svgContainer}>
-        <Image
+        {/* <Image
           src={semiCircle}
           alt="green semi circle"
           width={280}
           height={290}
           className={baseStyles.semiCircle}
-        />
+        /> */}
         <div className={baseStyles.successMan}>
           <Image src={successMan} alt="success man" width={280} height={290} />
           <Link href="/auth/login">Continuar</Link>
         </div>
       </article>
     </Modal>
-  );
+  )
 }
-export default Successful_Register;
+export default Successful_Register
